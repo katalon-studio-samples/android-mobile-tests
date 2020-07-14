70b8368ef3592014a0dc15222f71e59557fbceee
Duy Luong <duyluong@kms-technology.com>
2020-07-14 16:09:03 +0700

    Update script for v7.6

---
+++ Test Suites/Regression Tests v7.6.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression Tests v7.6</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e946e368-55a3-413b-9ef7-d73b553d73ac</testSuiteGuid>
+   <testCaseLink>
+      <guid>d7c583e4-808d-40d2-9567-39162be73f02</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/v7.6/Verify Last Items In List</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>587fc5f1-baae-4bb5-a41e-1a9e40a5d90c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/v7.6/Verify Correct Alarm Message</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
