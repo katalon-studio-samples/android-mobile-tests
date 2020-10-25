cc93b2b3482f32824be6cf4ef804746301a086d9
Luis Ramos <progluisram0s@gmail.com>
2020-10-25 03:32:08 -0600

    Automata
    Se agrega objeto dinamico para los viajes

--- Test Suites/Happy Path Transplace.ts
+++ Test Suites/Happy Path Transplace.ts
@@ -14,24 +14,6 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>bc941b41-8b76-442d-a524-c948a15800d5</testSuiteGuid>
    <testCaseLink>
-      <guid>5d2a7395-dc58-4018-a0da-b64cd72b7802</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Happy Path/Login</testCaseId>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>d27349bc-d235-4d91-a727-e4fde2fc1e95</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>0ee7e089-6ff2-40a8-a83c-caba5d2a33e6</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
       <guid>3348a159-6856-40fe-811f-a3845ec63d2f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
@@ -41,12 +23,60 @@
       <guid>d3561d55-2eab-4a58-89bd-960031cf9326</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Control/Transplace/Comenzar Viaje - T1 - Origen</testCaseId>
+      <testCaseId>Test Cases/Control/Transplace/Origen/Comenzar Viaje - T1 - Origen</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>8ed10c51-1c0a-49d7-930c-bafa23874df8</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Control/Transplace/Comenzar Viaje - T1 - Destino</testCaseId>
+      <testCaseId>Test Cases/Control/Transplace/Destinos/Comenzar Viaje - T1 - Destino</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d96ceb00-53b1-4b01-8f72-85de8acbcb76</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Control/Transplace/Origen/Comenzar Viaje - T2 - Origen</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>66024e72-6a2d-474f-9579-b766d3f8a2dc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Control/Transplace/Destinos/Comenzar Viaje - T2 - Destino</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>49100a1d-5b81-4fd6-be54-c4a7ea27884e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Control/Transplace/Origen/Comenzar Viaje - T3 - Origen</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a73e928b-c679-4f62-bebe-430bfcebb94a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Control/Transplace/Destinos/Comenzar Viaje - T3 - Destino</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f1d8e6f3-3bae-4f6b-b3ce-3da997587a4a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Control/Transplace/Origen/Comenzar Viaje - T4 - Origen</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>898eefda-bf0a-4760-b8f4-e0817b2ccafd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Control/Transplace/Destinos/Comenzar Viaje - T4 - Destino</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>00dff2ff-e903-4b42-be22-704f681a0e7f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Control/Transplace/Origen/Comenzar Viaje - T5 - Origen</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>81be73ca-b185-47a7-ad84-09df10806a6a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Control/Transplace/Destinos/Comenzar Viaje - T5 - Destino</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
