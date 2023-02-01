adbe0449b72faec300932619830e2307674f717a
Phu Vuong <phu.vuong@katalon.com>
2023-02-01 14:15:58 +0700

    Update Android Mobile Tests

--- Object Repository/Application/Graphics/android.widget.TextView - Xfermodes.rs
+++ Object Repository/Application/Graphics/android.widget.TextView - Xfermodes.rs
@@ -1,9 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
+<MobileElementEntity>
    <description></description>
    <name>android.widget.TextView - Xfermodes</name>
    <tag></tag>
-   <elementGuidId>7f335bf0-152c-4275-8c86-3bec01d6f977</elementGuidId>
+   <elementGuidId>00000000-0000-0000-0000-000000000000</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -11,13 +12,15 @@
       <name>class</name>
       <type>Main</type>
       <value>android.widget.TextView</value>
+      <webElementGuid>70dee6b2-6874-4b84-9629-5caf2ede68cd</webElementGuid>
    </webElementProperties>
    <webElementProperties>
-      <isSelected>true</isSelected>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
-      <name>instance</name>
+      <name>index</name>
       <type>Main</type>
-      <value>19</value>
+      <value>13</value>
+      <webElementGuid>d5843933-2d92-4222-a59f-6cffa63370d5</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -25,6 +28,7 @@
       <name>text</name>
       <type>Main</type>
       <value>Xfermodes</value>
+      <webElementGuid>6491ec06-1897-494b-8dc3-04ba27132078</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -32,13 +36,23 @@
       <name>resource-id</name>
       <type>Main</type>
       <value>android:id/text1</value>
+      <webElementGuid>a370623c-da3b-4b12-9d5f-0e56659fc619</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>package</name>
       <type>Main</type>
-      <value>com.hmh.api</value>
+      <value>io.appium.android.apis</value>
+      <webElementGuid>5080ce44-8384-46f1-8ac6-3a26f1fff1e5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>content-desc</name>
+      <type>Main</type>
+      <value>Xfermodes</value>
+      <webElementGuid>9a9c205c-e949-4a4e-962b-ef98eccc81b8</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -46,6 +60,7 @@
       <name>checkable</name>
       <type>Main</type>
       <value>false</value>
+      <webElementGuid>24812b06-3f9b-4f7e-a020-962b07e7c8c7</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -53,6 +68,7 @@
       <name>checked</name>
       <type>Main</type>
       <value>false</value>
+      <webElementGuid>9dd6a936-89b1-42d8-a0c2-0e11f5209987</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -60,6 +76,7 @@
       <name>clickable</name>
       <type>Main</type>
       <value>true</value>
+      <webElementGuid>39fff941-1611-4ecb-bfbd-a0b03f349a4d</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -67,6 +84,7 @@
       <name>enabled</name>
       <type>Main</type>
       <value>true</value>
+      <webElementGuid>b887eac8-93da-46ef-981e-7f0d55cd6f40</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -74,6 +92,7 @@
       <name>focusable</name>
       <type>Main</type>
       <value>false</value>
+      <webElementGuid>d4b509ce-4a5e-46ad-bc22-c61c4ee9500e</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -81,6 +100,7 @@
       <name>focused</name>
       <type>Main</type>
       <value>false</value>
+      <webElementGuid>c0186c1f-4ca2-44c9-8149-dc67b9821b42</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -88,6 +108,7 @@
       <name>scrollable</name>
       <type>Main</type>
       <value>false</value>
+      <webElementGuid>8052c62a-f5d2-4fba-9639-5cedc61493c0</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -95,6 +116,7 @@
       <name>long-clickable</name>
       <type>Main</type>
       <value>false</value>
+      <webElementGuid>9be56ed2-01b6-4694-bdda-d5ff86d0d22c</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -102,6 +124,7 @@
       <name>password</name>
       <type>Main</type>
       <value>false</value>
+      <webElementGuid>603e46e4-2f53-4c8f-bcf4-43ec62e9874e</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -109,6 +132,7 @@
       <name>selected</name>
       <type>Main</type>
       <value>false</value>
+      <webElementGuid>67edadd2-1e9b-4393-85fa-c98049a91f5c</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -116,26 +140,56 @@
       <name>x</name>
       <type>Main</type>
       <value>0</value>
+      <webElementGuid>73805093-76f2-476b-877a-403f263bea58</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>y</name>
       <type>Main</type>
-      <value>2434</value>
+      <value>2112</value>
+      <webElementGuid>03deab93-0965-4e28-8763-6c24580bffad</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>width</name>
       <type>Main</type>
-      <value>1440</value>
+      <value>1080</value>
+      <webElementGuid>1fdee812-449a-4999-bcb8-e9b6110b4e22</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>height</name>
       <type>Main</type>
-      <value>126</value>
+      <value>64</value>
+      <webElementGuid>520e6efd-def3-4e20-ab4e-df241de885ad</webElementGuid>
    </webElementProperties>
-</WebElementEntity>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>bounds</name>
+      <type>Main</type>
+      <value>[0,2112][1080,2176]</value>
+      <webElementGuid>6477a722-2a04-4c18-97b5-11c41570e6d4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>displayed</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>31b36719-7c91-4f5f-872e-f19ba743d327</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//hierarchy/android.widget.FrameLayout[1]/android.view.ViewGroup[1]/android.widget.FrameLayout[2]/android.widget.ListView[1]/android.widget.TextView[14]</value>
+      <webElementGuid>b6236fe0-2be2-444a-9b2a-69d9e363c9f9</webElementGuid>
+   </webElementProperties>
+   <locator>//*[@class = 'android.widget.TextView' and (@text = 'Xfermodes' or . = 'Xfermodes') and @resource-id = 'android:id/text1']</locator>
+   <locatorStrategy>ATTRIBUTES</locatorStrategy>
+</MobileElementEntity>
--- Scripts/Verify Correct Alarm Message/Script1529986178839.groovy
+++ Scripts/Verify Correct Alarm Message/Script1529986178839.groovy
@@ -1,25 +1,17 @@
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import internal.GlobalVariable as GlobalVariable
-import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
+
+import com.kms.katalon.core.configuration.RunConfiguration
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.util.internal.PathUtil as PathUtil
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.util.internal.PathUtil
+
+import internal.GlobalVariable
 
 Mobile.comment('Story: Verify correct alarm message')
 
 Mobile.comment('Given that user has started an application')
 
 'Get full directory\'s path of android application'
-def appPath = PathUtil.relativeToAbsolutePath(GlobalVariable.G_AndroidApp, RunConfiguration.getProjectDir())
+def appPath = PathUtil.relativeToAbsolutePath(GlobalVariable.G_AppPath, RunConfiguration.getProjectDir())
 
 Mobile.startApplication(appPath, false)
 
@@ -27,14 +19,14 @@ Mobile.comment('And he navigates the application to Activity form')
 
 Mobile.tap(findTestObject('Application/android.widget.TextView - App'), 10)
 
-Mobile.tap(findTestObject('Application/App/android.widget.TextView-Activity'), 10)
+Mobile.tap(findTestObject('Application/App/android.widget.TextView - Activity'), 10)
 
 Mobile.comment('When he taps on the Custom Dialog button')
 
-Mobile.tap(findTestObject('Application/App/Activity/android.widget.TextView-Custom Dialog'), 10)
+Mobile.tap(findTestObject('Application/App/Activity/android.widget.TextView - Custom Dialog'), 10)
 
 'Get displayed message on the dialog'
-def message = Mobile.getText(findTestObject('Application/App/Activity/Custom Dialog/android.widget.TextViewCustomDialog'), 
+def message = Mobile.getText(findTestObject('Application/App/Activity/Custom Dialog/android.widget.TextView - Message'), 
     10)
 
 Mobile.comment('Then the correct dialog message should be displayed')
